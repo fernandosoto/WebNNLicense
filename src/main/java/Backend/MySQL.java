8d6985559906344b1438b84cfc84c48135b608e4
Nightloyd <loyden.king@gmail.com>
2015-04-21 12:23:48 +0200

    Very basic addPurchase method.

--- src/main/java/Backend/MySQL.java
+++ src/main/java/Backend/MySQL.java
@@ -1,18 +1,47 @@
 package Backend;
 
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
 /**
  * Created by Fernando on 2015-04-21.
  */
-public class MySQL implements DBCommunication
-{
-    @Override
-    public void addLicense(License lic) {
-        
+public class MySQL implements DBCommunication {
+    Connection con = null;
+
+    public MySQL(String server) throws Exception {
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        con = DriverManager.getConnection(server);
     }
 
     @Override
-    public int addPurchase(Purchase pur) {
-        return 0;
+    public void addLicense(License lic) {
+
+    }
+
+    @Override
+    public int addPurchase(Purchase pur) throws Exception{
+        PreparedStatement stmt = null;
+        try {
+            String sql = "INSERT INTO PURCHASE(MANUFACTURER_ID, PRODUCT_NAME, TYPE, DISTRIBUTOR_ID, FREE_TEXT) VALUES('"
+                    + pur.getManufacturerId() + "', '" + pur.getProductName() + "', '"
+                    + pur.getType() + "', '" + pur.getDistributorId() + "', '" + pur.getFreeText() + "');";
+            stmt = con.prepareStatement(sql);
+            stmt.executeUpdate();
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return 1;
     }
 
     @Override
