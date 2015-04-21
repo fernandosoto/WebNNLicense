5cd587cce648eeafb24e5fd3684e49c259df9572
fernandosoto <fernandosoto_@hotmail.com>
2015-04-21 10:42:39 +0200

    Andra commit

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -4,13 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
 
 @Controller
 @RequestMapping("/")
 public class HelloController {
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
 		return "hello";
+	}
+
+	@RequestMapping("/time")
+	public String printTime(ModelMap model)
+	{
+		Date now = new Date();
+		model.addAttribute("message","Hello world the time is: " + now.toString());
+
+		return "time";
 	}
 }
---
+++ src/main/webapp/WEB-INF/pages/time.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Fernando
+  Date: 2015-04-21
+  Time: 10:21
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<h1>${message}</h1>
+</body>
+</html>
