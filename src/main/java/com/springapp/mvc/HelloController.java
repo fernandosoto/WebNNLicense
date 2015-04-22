47b7151afab13dfd3e2521ec0538a563a2bf392d
fernandosoto <fernandosoto_@hotmail.com>
2015-04-22 10:51:20 +0200

    Skapat form för att skicka data från websidan till java koden

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -1,7 +1,9 @@
 package com.springapp.mvc;
 
+import Backend.Purchase;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,15 +11,14 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.Date;
 
 @Controller
-@RequestMapping("/")
 public class HelloController {
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/",method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
+		model.addAttribute("message", "Add something");
+		model.addAttribute("purchase", new Purchase());
 		return "hello";
 	}
-
 	@RequestMapping("/time")
 	public String printTime(ModelMap model)
 	{
@@ -25,4 +26,11 @@ public class HelloController {
 		model.addAttribute("message","Hello world the time is: " + now.toString());
 		return "time";
 	}
+
+	@RequestMapping(value = "/",method = RequestMethod.POST)
+	public String printWelcome(@ModelAttribute Purchase purchase,ModelMap model) {
+		System.out.println(purchase.getProductName());
+		return "hello";
+	}
+
 }
