ddb86e32756d9d1cf74c40ccc7b7d5875fc2ae98
mattiasgrehnik <mattias.grehnik@gmail.com>
2015-04-29 11:47:45 +0200

    addPurchase now returns the autogenerated PURCHASE_ID

--- src/main/java/Backend/DAO/PurchaseDAOInterface.java
+++ src/main/java/Backend/DAO/PurchaseDAOInterface.java
@@ -9,7 +9,7 @@ import java.util.List;
  * Created by Isak on 2015-04-23.
  */
 public interface PurchaseDAOInterface {
-    void addPurchase(Purchase pur, String userName);
+    long addPurchase(Purchase pur, String userName);
 
     void deletePurchase(Purchase pur, String userName);
 
