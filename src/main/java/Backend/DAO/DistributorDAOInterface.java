bea9e3d033fbe550ccd2dfde82ee5baba5a85955
mattiasgrehnik <mattias.grehnik@gmail.com>
2015-04-29 14:46:30 +0200

    Added ugly quick fix for PurchaseDAO add #2.

--- src/main/java/Backend/DAO/DistributorDAOInterface.java
+++ src/main/java/Backend/DAO/DistributorDAOInterface.java
@@ -15,7 +15,7 @@ public interface DistributorDAOInterface {
 
     Distributor searchDistributorById(long id);
 
-    List<Distributor> searchDistributorByName(String name);
+    Distributor searchDistributorByName(String name);
 
     void setDataSource(DataSource dataSource);
 }
