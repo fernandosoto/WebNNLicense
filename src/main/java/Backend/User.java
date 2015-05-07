4be8075bfaa9578b3832ad3e8808f992b3a44f7d
mattiasgrehnik <mattias.grehnik@gmail.com>
2015-05-07 15:27:01 +0200

    Added login.jsp, loginController and User class

---
+++ src/main/java/Backend/User.java
@@ -0,0 +1,30 @@
+package Backend;
+
+/**
+ * Created by Anna on 2015-05-07.
+ */
+public class User {
+    private String userName;
+    private String password;
+
+    public User(String userName, String password){
+        this.userName=userName;
+        this.password=password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
---
+++ src/main/java/com/springapp/mvc/loginController.java
@@ -0,0 +1,16 @@
+package com.springapp.mvc;
+
+import Backend.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Anna on 2015-05-07.
+ */
+public class loginController {
+
+    private List<User> userList = new ArrayList<User>();
+
+
+}
