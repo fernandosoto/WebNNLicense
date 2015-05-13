9ec1d2faea921f23826659f93ee6a75319d6f2ad
fernandosoto <fernandosoto_@hotmail.com>
2015-05-13 13:58:09 +0200

    Rowmapper

---
+++ src/main/java/Backend/rowMapper/PurchaseRowMapper.java
@@ -0,0 +1,38 @@
+package Backend.rowMapper;
+
+import Backend.Purchase;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+/**
+ * Created by Fernando on 2015-05-13.
+ */
+@Component
+public class PurchaseRowMapper implements RowMapper{
+    public final static String PURCHASE_ID = "PURCHASE_ID";
+    public final static String MANUFACTURER_NAME = "MANUFACTURER_NAME";
+    public final static String PRODUCT_NAME = "PRODUCT_NAME";
+    public final static String LICENSE_TYPE = "LICENSE_TYPE";
+    public final static String DISTRIBUTOR_NAME ="DISTRIBUTOR_NAME";
+    public final static String FREE_TEXT = "FREE_TEXT";
+    public final static String UPGRADED_FROM = "UPGRADED_FROM";
+    public final static String CREATED_BY = "CREATED_BY";
+    public final static String CREATED_DATE = "CREATED_DATE";
+
+
+    public Purchase mapRow(ResultSet rs, int i) throws SQLException {
+        return new Purchase(rs.getLong(PURCHASE_ID),
+                rs.getString(MANUFACTURER_NAME),
+                rs.getString(PRODUCT_NAME),
+                rs.getString(LICENSE_TYPE),
+                rs.getString(DISTRIBUTOR_NAME),
+                rs.getString(FREE_TEXT),
+                rs.getString(CREATED_BY),
+                rs.getDate(CREATED_DATE));
+
+    }
+}
