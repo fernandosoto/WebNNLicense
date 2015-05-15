5241aa2aa8ed6605c22824fc39ec9c50b6057c71
fernandosoto <fernandosoto_@hotmail.com>
2015-05-15 12:52:51 +0200

    RowMapper och PurchaseDAO tester

--- src/main/java/Backend/DAO/PurchaseDAO.java
+++ src/main/java/Backend/DAO/PurchaseDAO.java
@@ -1,6 +1,8 @@
 package Backend.DAO;
 
 import Backend.Purchase;
+import Backend.rowMapper.PurchaseRowMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
@@ -19,12 +21,39 @@ import java.util.List;
  */
 @Service
 public class PurchaseDAO implements PurchaseDAOInterface {
-    private DataSource dataSource;
-    private JdbcTemplate db;
+    public DataSource dataSource;
+
+    @Autowired
+    public JdbcTemplate db;
+
+    @Autowired
+    PurchaseRowMapper purchaseRowMapper;
+
+
+
+
+    public static String SQL_SEARCH_BY_PRODUCT_NAME = "SELECT P.PURCHASE_ID, P.PRODUCT_NAME, P.LICENSE_TYPE, P.FREE_TEXT, D.DISTRIBUTOR_NAME, M.MANUFACTURER_NAME, CR.CREATED_BY,CR.CREATED_DATE " +
+            "FROM PURCHASE P, MANUFACTURER M, DISTRIBUTOR D,CREATOR CR " +
+            "WHERE M.MANUFACTURER_ID = P.MANUFACTURER_ID AND D.DISTRIBUTOR_ID = P.DISTRIBUTOR_ID " +
+            "AND P.PRODUCT_NAME LIKE ? " +
+            "AND CR.C_PURCHASE_ID = P.PURCHASE_ID " +
+            "AND P.PURCHASE_ID NOT IN (SELECT DP.D_PURCHASE_ID from DELETED_PURCHASE DP)";
+
+    public static String SQL_SEARCH_ALL_PRODUCT = "SELECT P.PURCHASE_ID, P.PRODUCT_NAME, P.LICENSE_TYPE, P.FREE_TEXT, D.DISTRIBUTOR_NAME, M.MANUFACTURER_NAME, CR.CREATED_BY,CR.CREATED_DATE " +
+            "FROM PURCHASE P, MANUFACTURER M, DISTRIBUTOR D,CREATOR CR " +
+            "WHERE M.MANUFACTURER_ID = P.MANUFACTURER_ID AND D.DISTRIBUTOR_ID = P.DISTRIBUTOR_ID " +
+            "AND CR.C_PURCHASE_ID = P.PURCHASE_ID " +
+            "AND P.PURCHASE_ID NOT IN (SELECT DP.D_PURCHASE_ID from DELETED_PURCHASE DP)";
+    private JdbcTemplate jdbcTemplate;
 
     @Transactional
-    public long addPurchase(final Purchase pur, final String userName, final long distrId, final long manuId) {
+    public long addPurchase(final Purchase pur, final String userName, final long distrId, final long manuId) throws Exception {
         final KeyHolder holder = new GeneratedKeyHolder();
+        System.out.println("Mockito suger balle");
+        if(userName == null){
+            System.out.println("You done goofed");
+            throw new Exception("You done goofed");
+        }
 
         db.update(new PreparedStatementCreator() {
             @Override
@@ -35,6 +64,8 @@ public class PurchaseDAO implements PurchaseDAOInterface {
                 ps.setString(3, pur.getType());
                 ps.setLong(4, distrId);
                 ps.setString(5, pur.getFreeText());
+                               //+ manuId + ", '" + pur.getProductName() + "', '"
+                                //+ pur.getType() + "', " + distrId + ", '" + pur.getFreeText() + "');";)
                 return ps;
             }
         }, holder);
@@ -49,11 +80,12 @@ public class PurchaseDAO implements PurchaseDAOInterface {
                 return ps;
             }
         });
-        return holder.getKey().longValue();
+        return holder.getKey().longValue();// holder.getKey().longValue();
     }
 
     @Override
     public void deletePurchase(final Purchase pur, final String userName) {
+
        db.update(new PreparedStatementCreator() {
             @Override
             public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
@@ -64,10 +96,17 @@ public class PurchaseDAO implements PurchaseDAOInterface {
                 return ps;
             }
         });
+//        String sql = "INSERT INTO DELETED_PURCHASE(DELETED_BY, DELETED_DATE, PURCHASE_ID) VALUES ('" + userName + "', "
+//                + new Date(System.currentTimeMillis()) + ", " + pur.getPurchaseId() + ");";
+//        db.update(sql);
     }
 
     public List<Purchase> searchDeletedPurchases(){
-       List<Purchase> p = db.query(new PreparedStatementCreator() {
+//        String sql = "SELECT P.PURCHASE_ID, P.PRODUCT_NAME, P.LICENSE_TYPE, DP.DELETED_DATE, DP.DELETED_BY, P.FREE_TEXT, D.DISTRIBUTOR_NAME, M.MANUFACTURER_NAME" +
+//                " FROM PURCHASE P, MANUFACTURER M, DISTRIBUTOR D, DELETED_PURCHASE DP" +
+//                " WHERE DP.PURCHASE_ID != P.PURCHASE_ID;";
+
+        List<Purchase> p = db.query(new PreparedStatementCreator() {
             @Override
             public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
                 PreparedStatement ps = connection.prepareStatement("SELECT P.PURCHASE_ID, P.PRODUCT_NAME, P.LICENSE_TYPE, DP.DELETED_DATE, DP.DELETED_BY, P.FREE_TEXT, D.DISTRIBUTOR_NAME, M.MANUFACTURER_NAME" +
@@ -87,7 +126,12 @@ public class PurchaseDAO implements PurchaseDAOInterface {
     }
 
     public Purchase searchPurchaseById(final long id) {
-       return db.query(new PreparedStatementCreator() {
+//        String sql = "SELECT P.PURCHASE_ID, P.PRODUCT_NAME, P.LICENSE_TYPE, P.FREE_TEXT, D.DISTRIBUTOR_NAME, M.MANUFACTURER_NAME, C.CREATED_BY, C.CREATED_DATE" +
+//                " FROM PURCHASE P, MANUFACTURER M, DISTRIBUTOR D, CREATOR C, DELETED_PURCHASE DP" +
+//                " WHERE M.MANUFACTURER_ID = P.MANUFACTURER_ID AND D.DISTRIBUTOR_ID = P.DISTRIBUTOR_ID" +
+//                " AND P.PURCHASE_ID = " + id + " AND DP.PURCHASE_ID != P.PURCHASE_ID" + " AND P.PURCHASE_ID = " + " C.PURCHASE_ID;";
+
+        return db.query(new PreparedStatementCreator() {
                 @Override
             public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
                 PreparedStatement ps = connection.prepareStatement("SELECT P.PURCHASE_ID, P.PRODUCT_NAME, P.LICENSE_TYPE, P.FREE_TEXT, D.DISTRIBUTOR_NAME, M.MANUFACTURER_NAME, CR.CREATED_BY,CR.CREATED_DATE " +
@@ -109,32 +153,42 @@ public class PurchaseDAO implements PurchaseDAOInterface {
         }).get(0);
     }
 
-    public List<Purchase> searchPurchaseByName(final String name) {
-        List<Purchase> p = db.query(new PreparedStatementCreator() {
+    public List<Purchase> searchPurchaseByName(String product_name) throws Exception {
+//        String sql = "SELECT P.PURCHASE_ID, P.PRODUCT_NAME, P.LICENSE_TYPE, P.FREE_TEXT, D.DISTRIBUTOR_NAME, M.MANUFACTURER_NAME" +
+//                " FROM PURCHASE P, MANUFACTURER M, DISTRIBUTOR D, DELETED_PURCHASE DP" +
+//                " WHERE M.MANUFACTURER_ID = P.MANUFACTURER_ID AND D.DISTRIBUTOR_ID = P.DISTRIBUTOR_ID" +
+//                " AND P.PRODUCT_NAME LIKE '" + name + "%'" + " AND DP.PURCHASE_ID != P.PURCHASE_ID" + " AND P.PURCHASE_ID = " + " C.PURCHASE_ID;";
+
+       /* List<Purchase> p = db.query(new PreparedStatementCreator() {
             @Override
             public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                PreparedStatement ps = connection.prepareStatement("SELECT P.PURCHASE_ID, P.PRODUCT_NAME, P.LICENSE_TYPE, P.FREE_TEXT, D.DISTRIBUTOR_NAME, M.MANUFACTURER_NAME, CR.CREATED_BY,CR.CREATED_DATE " +
-                                                "FROM PURCHASE P, MANUFACTURER M, DISTRIBUTOR D,CREATOR CR " +
-                                                "WHERE M.MANUFACTURER_ID = P.MANUFACTURER_ID AND D.DISTRIBUTOR_ID = P.DISTRIBUTOR_ID " +
-                                                "AND P.PRODUCT_NAME LIKE ? " +
-                                                "AND CR.C_PURCHASE_ID = P.PURCHASE_ID " +
-                                                "AND P.PURCHASE_ID NOT IN (SELECT DP.D_PURCHASE_ID from DELETED_PURCHASE DP)");
+                PreparedStatement ps = connection.prepareStatement(SQL_SEARCH_BY_PRODUCT_NAME);
                 ps.setString(1, name+"%");
                 return ps;
             }
-        }, new RowMapper<Purchase>() {
+        },new PurchaseRowMapper());*/
+        if(product_name == null)
+            throw new Exception("Need a product name!");
+
+       return db.query(SQL_SEARCH_BY_PRODUCT_NAME, purchaseRowMapper,product_name);
+       /* new RowMapper<Purchase>() {
             @Override
             public Purchase mapRow(ResultSet rs, int i) throws SQLException {
                 return new Purchase(rs.getLong("PURCHASE_ID"), rs.getString("MANUFACTURER_NAME"), rs.getString("PRODUCT_NAME"),
                         rs.getString("LICENSE_TYPE"), rs.getString("DISTRIBUTOR_NAME"), rs.getString("FREE_TEXT"), getUpgradedFrom(rs.getLong("PURCHASE_ID")),
                         rs.getString("CREATED_BY"), rs.getDate("CREATED_DATE"));
             }
-        });
-        return p;
+        }*/
+        //return p;
     }
 
     public List<Purchase> searchPurchaseByDistributorName(final String name) {
-         List<Purchase> p = db.query(new PreparedStatementCreator() {
+//        String sql = "SELECT P.PURCHASE_ID, P.PRODUCT_NAME, P.LICENSE_TYPE, P.FREE_TEXT, D.DISTRIBUTOR_NAME, M.MANUFACTURER_NAME, U.ORIGINAL_PURCHASE_ID" +
+//                " FROM PURCHASE P, MANUFACTURER M, DISTRIBUTOR D, UPGRADED_PURCHASE U, DELETED_PURCHASE DP" +
+//                " WHERE M.MANUFACTURER_ID = P.MANUFACTURER_ID AND D.DISTRIBUTOR_ID = P.DISTRIBUTOR_ID" +
+//                " AND D.DISTRIBUTOR_NAME LIKE '" + name + "%'" + " AND DP.PURCHASE_ID != P.PURCHASE_ID" + " AND P.PURCHASE_ID = " + " C.PURCHASE_ID;";
+
+        List<Purchase> p = db.query(new PreparedStatementCreator() {
             @Override
             public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
                 PreparedStatement ps = connection.prepareStatement("SELECT P.PURCHASE_ID, P.PRODUCT_NAME, P.LICENSE_TYPE, P.FREE_TEXT, D.DISTRIBUTOR_NAME, M.MANUFACTURER_NAME, U.ORIGINAL_PURCHASE_ID" +
@@ -156,6 +210,12 @@ public class PurchaseDAO implements PurchaseDAOInterface {
     }
 
     public List<Purchase> searchPurchaseByManufacturerName(final String name) {
+//        String sql = "SELECT P.PURCHASE_ID, P.PRODUCT_NAME, P.LICENSE_TYPE, P.FREE_TEXT, D.DISTRIBUTOR_NAME, M.MANUFACTURER_NAME" +
+//                " FROM PURCHASE P, MANUFACTURER M, DISTRIBUTOR D, DELETED_PURCHASE DP" +
+//                " WHERE M.MANUFACTURER_ID = P.MANUFACTURER_ID AND D.DISTRIBUTOR_ID = P.DISTRIBUTOR_ID" +
+//                " AND M.MANUFACTURER_NAME LIKE '" + name + "%'" + " AND DP.PURCHASE_ID != P.PURCHASE_ID" + " AND P.PURCHASE_ID = " + " C.PURCHASE_ID;";
+
+
         List<Purchase> p = db.query(new PreparedStatementCreator() {
             @Override
             public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
@@ -178,6 +238,11 @@ public class PurchaseDAO implements PurchaseDAOInterface {
     }
 
     public List<Purchase> searchPurchaseByType(final String type) {
+//        String sql = "SELECT P.PURCHASE_ID, P.PRODUCT_NAME, P.LICENSE_TYPE, P.FREE_TEXT, D.DISTRIBUTOR_NAME, M.MANUFACTURER_NAME" +
+//                " FROM PURCHASE P, MANUFACTURER M, DISTRIBUTOR D, DELETED_PURCHASE DP" +
+//                " WHERE M.MANUFACTURER_ID = P.MANUFACTURER_ID AND D.DISTRIBUTOR_ID = P.DISTRIBUTOR_ID" +
+//                " AND P.LICENSE_TYPE LIKE '" + type + "%'" + " AND P.PURCHASE_ID = " + " C.PURCHASE_ID;";
+
         List<Purchase> p = db.query(new PreparedStatementCreator() {
             @Override
             public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
@@ -198,29 +263,19 @@ public class PurchaseDAO implements PurchaseDAOInterface {
         });
         return p;
     }
-    @Override
-    public void editPurchase(final Purchase pur, final String userName, final long manufacturerId, final long distributorId){
-        final Purchase oldPur = searchPurchaseById(pur.getPurchaseId());
 
-        db.update(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                PreparedStatement ps = connection.prepareStatement("UPDATE PURCHASE SET PRODUCT_NAME = ?"+
-                        ", LICENSE_TYPE = ? ,MANUFACTURER_ID = ?,DISTRIBUTOR_ID = ?, FREE_TEXT = ? " +
-                        " WHERE PURCHASE_ID = ?");
-                ps.setString(1, pur.getProductName());
-                ps.setString(2, pur.getType());
-                ps.setLong(3, manufacturerId);
-                ps.setLong(4, distributorId);
-                ps.setString(5, pur.getFreeText());
-                ps.setLong(6, oldPur.getPurchaseId());
-                return ps;
-            }
-        });
+    public List<Purchase> searchAllPurchases(){
+        System.out.println("SUp");
+        return db.query(SQL_SEARCH_ALL_PRODUCT, purchaseRowMapper);
+    }
+
+    @Override
+    public void editPurchase(Purchase pur, String userName, long manufacturerId, long distributorId) {
 
     }
 
-    private long getUpgradedFrom(long newPurchaseId) {
+
+    public long getUpgradedFrom(long newPurchaseId) {
         long id;
         try {
             id = db.queryForLong("SELECT ORIGINAL_PURCHASE_ID FROM UPGRADED_PURCHASE WHERE NEW_PURCHASE_ID = " + newPurchaseId + ";");
@@ -231,8 +286,16 @@ public class PurchaseDAO implements PurchaseDAOInterface {
 
     }
 
-    public void setDataSource(DataSource dataSource) {
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    /*public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
         db = new JdbcTemplate(dataSource);
-    }
+    }*/
 }
---
+++ src/test/java/com/springapp/mvc/PurchaseDAOTest.java
@@ -0,0 +1,130 @@
+package com.springapp.mvc;
+
+import Backend.*;
+import Backend.DAO.LicenseDAO;
+import Backend.DAO.LicenseDAOInterface;
+import Backend.DAO.PurchaseDAO;
+import Backend.DAO.PurchaseDAOInterface;
+import Backend.rowMapper.PurchaseRowMapper;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static org.mockito.Matchers.*;
+
+/**
+ * Created by Fernando on 2015-05-08.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
+public class PurchaseDAOTest {
+
+    @Autowired
+    protected WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Mock
+    JdbcTemplate jdbcTemplate;
+
+    @Mock
+    PurchaseRowMapper purchaseRowMapper;
+
+    @InjectMocks
+    PurchaseDAO pdao;
+
+    Purchase p1,p2;
+
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = webAppContextSetup(this.wac).build();
+        p1 = new Purchase();
+        p1.setProductName("XP");
+        p1.setCreatedDate(new Date(System.currentTimeMillis()));
+        p1.setDistributorName("Komplett");
+        p1.setManufacturerName("Microsoft");
+        p1.setType("os");
+        p1.setFreeText("Bra grejer");
+
+        p2 = new Purchase();
+        p2.setProductName("redfox");
+        p2.setCreatedDate(new Date(System.currentTimeMillis()));
+        p2.setDistributorName("Komplett");
+        p2.setManufacturerName("Linux");
+        p2.setType("server");
+        p2.setFreeText("Bra server");
+
+    }
+
+    @Test
+    public void addViewTest()throws Exception{
+        mockMvc.perform(get("/addPurchase"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("add/add_inner"));
+
+    }
+
+
+    @Test
+    public void searchDAOTest()throws Exception{
+
+        List<Purchase> expected = new ArrayList<Purchase>();
+        expected.add(p1);
+        System.out.println(expected.get(0).getProductName());
+        when(jdbcTemplate.query(pdao.SQL_SEARCH_BY_PRODUCT_NAME, purchaseRowMapper, expected.get(0).getProductName())).thenReturn(expected);
+        //when(db.query(anyString(),(RowMapper<Purchase>) anyObject(), anyString())).thenReturn(expected);
+        //when(jdbcTemplate.query(pdao.SQL_SEARCH_ALL_PRODUCT,purchaseRowMapper)).thenReturn(expected);
+        //assertEquals(expected, pdao.searchAllPurchases());
+        assertEquals(expected, pdao.searchPurchaseByName(p1.getProductName()));
+    }
+
+    @Test(expected = DataAccessException.class)
+    public void searchNameDAOAccessExceptionTest()throws Exception{
+        when(jdbcTemplate.query(pdao.SQL_SEARCH_BY_PRODUCT_NAME,purchaseRowMapper,p1.getProductName())).thenThrow(mock(DataAccessException.class));
+
+        pdao.searchPurchaseByName(p1.getProductName());
+    }
+
+    @Test(expected = Exception.class)
+    public void searchNameDAONameExceptionTest()throws Exception{
+        when(jdbcTemplate.query(pdao.SQL_SEARCH_BY_PRODUCT_NAME,purchaseRowMapper,null)).thenThrow(mock(Exception.class));
+
+        pdao.searchPurchaseByName(null);
+    }
+
+
+
+}
