3b7da709a2544b3c58c129b8afe58d7b665c6295
mattiasgrehnik <mattias.grehnik@gmail.com>
2015-05-18 13:28:18 +0200

    Added DeletedPurchase and DeletedPurchaseRowMapper.

--- src/main/java/Backend/DeletedPurchase.java
+++ src/main/java/Backend/DeletedPurchase.java
@@ -11,48 +11,39 @@ public class DeletedPurchase extends Purchase{
     private long id;
     private String deletedBy;
     private Date deletedDate;
-    private long deletedPurchaseId;
 
-    public DeletedPurchase(long id, String deletedBy, Date deletedDate){
+    public DeletedPurchase(long purchaseId, String manufacturerName, String productName, String type, String distributorName,
+                           String freeText, long upgradeFrom, String createdBy, Date createdDate, long id, String deletedBy,
+                           Date deletedDate){
+
+        super(purchaseId, manufacturerName, productName, type, distributorName,
+                freeText, upgradeFrom, createdBy, createdDate);
         this.id = id;
         this.deletedBy = deletedBy;
         this.deletedDate = deletedDate;
-        deletedPurchaseId = super.getPurchaseId();
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
+    public void setId(long id){
         this.id = id;
     }
 
-    @Override
+    public long getId(){
+        return id;
+    }
+
     public String getDeletedBy() {
         return deletedBy;
     }
 
-    @Override
     public void setDeletedBy(String deletedBy) {
         this.deletedBy = deletedBy;
     }
 
-    @Override
     public Date getDeletedDate() {
         return deletedDate;
     }
 
-    @Override
     public void setDeletedDate(Date deletedDate) {
         this.deletedDate = deletedDate;
-    }
-
-    public long getDeletedPurchaseId() {
-        return deletedPurchaseId;
-    }
-
-    public void setDeletedPurchaseId() {
-        this.deletedPurchaseId = super.getPurchaseId();
     }
 }
---
+++ src/main/java/Backend/rowMapper/DeletedPurchaseRowMapper.java
@@ -0,0 +1,41 @@
+package Backend.rowMapper;
+
+import Backend.DeletedPurchase;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by Isak on 2015-05-18.
+ */
+public class DeletedPurchaseRowMapper {
+    public final static String PURCHASE_ID = "PURCHASE_ID";
+    public final static String MANUFACTURER_NAME = "MANUFACTURER_NAME";
+    public final static String PRODUCT_NAME = "PRODUCT_NAME";
+    public final static String LICENSE_TYPE = "LICENSE_TYPE";
+    public final static String DISTRIBUTOR_NAME ="DISTRIBUTOR_NAME";
+    public final static String FREE_TEXT = "FREE_TEXT";
+    public final static String UPGRADED_FROM = "UPGRADED_FROM";
+    public final static String CREATED_BY = "CREATED_BY";
+    public final static String CREATED_DATE = "CREATED_DATE";
+    public final static String DELETED_ID = "DELETED_ID";
+    public final static String DELETED_BY = "DELETED_BY";
+    public final static String DELETED_DATE = "DELETED_DATE";
+
+    public DeletedPurchase mapRow(ResultSet rs, int i) throws SQLException {
+        DeletedPurchase deletedPurchase = new DeletedPurchase(rs.getLong(PURCHASE_ID),
+                rs.getString(MANUFACTURER_NAME),
+                rs.getString(PRODUCT_NAME),
+                rs.getString(LICENSE_TYPE),
+                rs.getString(DISTRIBUTOR_NAME),
+                rs.getString(FREE_TEXT),
+                rs.getLong(UPGRADED_FROM),
+                rs.getString(CREATED_BY),
+                rs.getDate(CREATED_DATE),
+                rs.getLong(DELETED_ID),
+                rs.getString(DELETED_BY),
+                rs.getDate(DELETED_DATE));
+
+        return deletedPurchase;
+    }
+}
