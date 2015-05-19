fa0f324a41d6d84ea4776419c0be652fb512d399
fernandosoto <fernandosoto_@hotmail.com>
2015-05-19 12:54:44 +0200

    PurchaseDAO fixade searchDeleted genom att lägga till UPGRADED_PURCHASE och deletedPurchaseRowMapper

--- src/main/java/Backend/rowMapper/DeletedPurchaseRowMapper.java
+++ src/main/java/Backend/rowMapper/DeletedPurchaseRowMapper.java
@@ -16,7 +16,7 @@ public class DeletedPurchaseRowMapper implements RowMapper{
     public final static String LICENSE_TYPE = "LICENSE_TYPE";
     public final static String DISTRIBUTOR_NAME ="DISTRIBUTOR_NAME";
     public final static String FREE_TEXT = "FREE_TEXT";
-    public final static String UPGRADED_FROM = "ORIGIGNAL_PURCHASE_ID";
+    public final static String UPGRADED_FROM = "ORIGINAL_PURCHASE_ID";
     public final static String CREATED_BY = "CREATED_BY";
     public final static String CREATED_DATE = "CREATED_DATE";
     public final static String DELETED_ID = "DELETED_ID";
