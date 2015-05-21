17658d292df0a9de7e3fab513517e1f1ec4a98c1
Nightloyd <loyden.king@gmail.com>
2015-05-21 11:57:55 +0200

    Added so exceptions are now recorded in the logger.

--- src/main/java/Backend/DAO/PurchaseDAO.java
+++ src/main/java/Backend/DAO/PurchaseDAO.java
@@ -1,10 +1,13 @@
 package Backend.DAO;
 
+import Backend.ContextListener;
 import Backend.DeletedPurchase;
 import Backend.Purchase;
 import Backend.rowMapper.DeletedPurchaseRowMapper;
 import Backend.rowMapper.PurchaseRowMapper;
+import org.mockito.internal.matchers.Null;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
@@ -35,6 +38,148 @@ public class PurchaseDAO implements PurchaseDAOInterface {
     @Autowired
     DeletedPurchaseRowMapper deletedPurchaseRowMapper;
 
+    @Transactional
+    public long addPurchase(final Purchase pur, final String userName, final long distrId, final long manuId) throws Exception {
+        final KeyHolder holder = new GeneratedKeyHolder();
+        if(userName == null){
+            throw new IllegalArgumentException(userName);
+        }
+
+        db.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement("INSERT INTO PURCHASE(MANUFACTURER_ID, PRODUCT_NAME, LICENSE_TYPE, DISTRIBUTOR_ID, FREE_TEXT) VALUES(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+                ps.setLong(1, manuId);
+                ps.setString(2, pur.getProductName());
+                ps.setString(3, pur.getType());
+                ps.setLong(4, distrId);
+                ps.setString(5, pur.getFreeText());
+                return ps;
+            }
+        }, holder);
+
+        db.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement("INSERT INTO CREATOR(CREATED_BY, CREATED_DATE, C_PURCHASE_ID) VALUES(?, ?, ?)");
+                ps.setString(1, userName);
+                ps.setDate(2, new Date(System.currentTimeMillis()));
+                ps.setLong(3, holder.getKey().longValue());
+                return ps;
+            }
+        });
+        return holder.getKey().longValue();
+    }
+
+    @Override
+    public void deletePurchase(final Purchase pur, final String userName) {
+
+       db.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement("INSERT INTO DELETED_PURCHASE(DELETED_BY, DELETED_DATE, D_PURCHASE_ID) VALUES(?, ?, ?)");
+                ps.setString(1, userName);
+                ps.setDate(2, new Date(System.currentTimeMillis()));
+                ps.setLong(3, pur.getPurchaseId());
+                return ps;
+            }
+        });
+    }
+
+    public List<DeletedPurchase> searchDeletedPurchases(){
+
+        return db.query(SQL_SEARCH_ALL_DELETED, deletedPurchaseRowMapper);
+    }
+
+    public Purchase searchPurchaseById(long id) {
+        if(id == 0) {
+            ContextListener.log.error("I'd must be separate from 0, Id is : " + id, new IllegalArgumentException());
+            throw new IllegalArgumentException("Id must be separate from 0, Id is : " + id);
+        }
+        try {
+            return (Purchase) db.query(SQL_SEARCH_BY_ID,purchaseRowMapper,id).get(0);
+        } catch (DataAccessException e){
+            ContextListener.log.error("DataAccessException, probably database connection error.", e);
+            throw e;
+        } catch (NullPointerException e){
+            ContextListener.log.error("searchPurchaseById returned empty list with id : " + id, e);
+            throw e;
+        }
+    }
+
+    public List<Purchase> searchPurchaseByName(String product_name) {
+        if(product_name == null)
+            throw new IllegalArgumentException(product_name);
+
+        return db.query(SQL_SEARCH_BY_PRODUCT_NAME, purchaseRowMapper,product_name+"%");
+    }
+
+    public List<Purchase> searchPurchaseByDistributorName(String name) {
+        if(name == null)
+            throw new IllegalArgumentException(name);
+
+        return db.query(SQL_SEARCH_BY_DISTRIBUTOR,purchaseRowMapper,name+"%");
+    }
+
+    public List<Purchase> searchPurchaseByManufacturerName(String name) {
+        if(name == null)
+            throw new IllegalArgumentException(name);
+
+        return db.query(SQL_SEARCH_BY_MANUFACTURER, purchaseRowMapper, name + "%");
+    }
+
+    public List<Purchase> searchPurchaseByType(String type) {
+        if(type == null)
+            throw new IllegalArgumentException(type);
+
+        return db.query(SQL_SEARCH_BY_TYPE, purchaseRowMapper, type+"%");
+    }
+
+    public List<Purchase> searchAllPurchases(){
+        return db.query(SQL_SEARCH_ALL_PRODUCT, purchaseRowMapper);
+    }
+
+    @Override
+    public void editPurchase(final Purchase pur, final String userName, final long manufacturerId, final long distributorId) {
+        final Purchase oldPur = searchPurchaseById(pur.getPurchaseId());
+
+        db.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement ps = connection.prepareStatement("UPDATE PURCHASE SET PRODUCT_NAME = ?" +
+                        ", LICENSE_TYPE = ? ,MANUFACTURER_ID = ?,DISTRIBUTOR_ID = ?, FREE_TEXT = ? " +
+                        " WHERE PURCHASE_ID = ?");
+                ps.setString(1, pur.getProductName());
+                ps.setString(2, pur.getType());
+                ps.setLong(3, manufacturerId);
+                ps.setLong(4, distributorId);
+                ps.setString(5, pur.getFreeText());
+                ps.setLong(6, oldPur.getPurchaseId());
+                return ps;
+            }
+        });
+    }
+
+    public DeletedPurchase searchDeletedPurchaseById(long id)
+    {
+        return (DeletedPurchase) db.query(SQL_SEARCH_DELETED_BY_ID,deletedPurchaseRowMapper,id).get(0);
+    }
+
+    public long getUpgradedFrom(long newPurchaseId) {
+        long id;
+        try {
+            id = db.queryForLong("SELECT ORIGINAL_PURCHASE_ID FROM UPGRADED_PURCHASE WHERE NEW_PURCHASE_ID = " + newPurchaseId + ";");
+        } catch (IncorrectResultSizeDataAccessException e) {
+            return 0;
+        }
+        return id;
+
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+        db = new JdbcTemplate(dataSource);
+    }
 
     public static final String SQL_SEARCH_BY_PRODUCT_NAME = "SELECT P.PURCHASE_ID, P.PRODUCT_NAME, P.LICENSE_TYPE, P.FREE_TEXT, D.DISTRIBUTOR_NAME, M.MANUFACTURER_NAME, CR.CREATED_BY,CR.CREATED_DATE " +
             "FROM PURCHASE P " +
@@ -107,143 +252,4 @@ public class PurchaseDAO implements PurchaseDAOInterface {
             "LEFT OUTER JOIN UPGRADED_PURCHASE UP ON UP.NEW_PURCHASE_ID = P.PURCHASE_ID "+
             "WHERE P.PURCHASE_ID = ?";
 
-    @Transactional
-    public long addPurchase(final Purchase pur, final String userName, final long distrId, final long manuId) throws Exception {
-        final KeyHolder holder = new GeneratedKeyHolder();
-        if(userName == null){
-            throw new IllegalArgumentException(userName);
-        }
-
-        db.update(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                PreparedStatement ps = connection.prepareStatement("INSERT INTO PURCHASE(MANUFACTURER_ID, PRODUCT_NAME, LICENSE_TYPE, DISTRIBUTOR_ID, FREE_TEXT) VALUES(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
-                ps.setLong(1, manuId);
-                ps.setString(2, pur.getProductName());
-                ps.setString(3, pur.getType());
-                ps.setLong(4, distrId);
-                ps.setString(5, pur.getFreeText());
-                               //+ manuId + ", '" + pur.getProductName() + "', '"
-                                //+ pur.getType() + "', " + distrId + ", '" + pur.getFreeText() + "');";)
-                return ps;
-            }
-        }, holder);
-
-        db.update(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                PreparedStatement ps = connection.prepareStatement("INSERT INTO CREATOR(CREATED_BY, CREATED_DATE, C_PURCHASE_ID) VALUES(?, ?, ?)");
-                ps.setString(1, userName);
-                ps.setDate(2, new Date(System.currentTimeMillis()));
-                ps.setLong(3, holder.getKey().longValue());
-                return ps;
-            }
-        });
-        return holder.getKey().longValue();// holder.getKey().longValue();
-    }
-
-    @Override
-    public void deletePurchase(final Purchase pur, final String userName) {
-
-       db.update(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                PreparedStatement ps = connection.prepareStatement("INSERT INTO DELETED_PURCHASE(DELETED_BY, DELETED_DATE, D_PURCHASE_ID) VALUES(?, ?, ?)");
-                ps.setString(1, userName);
-                ps.setDate(2, new Date(System.currentTimeMillis()));
-                ps.setLong(3, pur.getPurchaseId());
-                return ps;
-            }
-        });
-//        String sql = "INSERT INTO DELETED_PURCHASE(DELETED_BY, DELETED_DATE, PURCHASE_ID) VALUES ('" + userName + "', "
-//                + new Date(System.currentTimeMillis()) + ", " + pur.getPurchaseId() + ");";
-//        db.update(sql);
-    }
-
-    public List<DeletedPurchase> searchDeletedPurchases(){
-
-        return db.query(SQL_SEARCH_ALL_DELETED, deletedPurchaseRowMapper);
-    }
-
-    public Purchase searchPurchaseById(long id) {
-        if(id == 0)
-            throw new IllegalArgumentException("Id must be separate from 0, Id is : " + id);
-
-        return (Purchase) db.query(SQL_SEARCH_BY_ID,purchaseRowMapper,id).get(0);
-
-    }
-
-    public List<Purchase> searchPurchaseByName(String product_name) {
-        if(product_name == null)
-            throw new IllegalArgumentException(product_name);
-
-        return db.query(SQL_SEARCH_BY_PRODUCT_NAME, purchaseRowMapper,product_name+"%");
-    }
-
-    public List<Purchase> searchPurchaseByDistributorName(String name) {
-        if(name == null)
-            throw new IllegalArgumentException(name);
-
-        return db.query(SQL_SEARCH_BY_DISTRIBUTOR,purchaseRowMapper,name+"%");
-    }
-
-    public List<Purchase> searchPurchaseByManufacturerName(String name) {
-        if(name == null)
-            throw new IllegalArgumentException(name);
-
-        return db.query(SQL_SEARCH_BY_MANUFACTURER, purchaseRowMapper,name+"%");
-    }
-
-    public List<Purchase> searchPurchaseByType(String type) {
-        if(type == null)
-            throw new IllegalArgumentException(type);
-
-        return db.query(SQL_SEARCH_BY_TYPE, purchaseRowMapper, type+"%");
-    }
-
-    public List<Purchase> searchAllPurchases(){
-        return db.query(SQL_SEARCH_ALL_PRODUCT, purchaseRowMapper);
-    }
-
-    @Override
-    public void editPurchase(final Purchase pur, final String userName, final long manufacturerId, final long distributorId) {
-        final Purchase oldPur = searchPurchaseById(pur.getPurchaseId());
-
-        db.update(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                PreparedStatement ps = connection.prepareStatement("UPDATE PURCHASE SET PRODUCT_NAME = ?" +
-                        ", LICENSE_TYPE = ? ,MANUFACTURER_ID = ?,DISTRIBUTOR_ID = ?, FREE_TEXT = ? " +
-                        " WHERE PURCHASE_ID = ?");
-                ps.setString(1, pur.getProductName());
-                ps.setString(2, pur.getType());
-                ps.setLong(3, manufacturerId);
-                ps.setLong(4, distributorId);
-                ps.setString(5, pur.getFreeText());
-                ps.setLong(6, oldPur.getPurchaseId());
-                return ps;
-            }
-        });
-    }
-
-    public DeletedPurchase searchDeletedPurchaseById(long id)
-    {
-        return (DeletedPurchase) db.query(SQL_SEARCH_DELETED_BY_ID,deletedPurchaseRowMapper,id).get(0);
-    }
-
-    public long getUpgradedFrom(long newPurchaseId) {
-        long id;
-        try {
-            id = db.queryForLong("SELECT ORIGINAL_PURCHASE_ID FROM UPGRADED_PURCHASE WHERE NEW_PURCHASE_ID = " + newPurchaseId + ";");
-        } catch (IncorrectResultSizeDataAccessException e) {
-            return 0;
-        }
-        return id;
-
-    }
-
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
-        db = new JdbcTemplate(dataSource);
-    }
 }
