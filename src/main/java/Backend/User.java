0f378ca8e0b8415c2c1302af9e067da0c10fad53
fernandosoto <fernandosoto_@hotmail.com>
2015-05-29 14:10:05 +0200

    User javadocs

--- src/main/java/Backend/User.java
+++ src/main/java/Backend/User.java
@@ -8,39 +8,80 @@ public class User {
     private String password;
     private static String LoggedInUser;
 
+    /**
+     * Empty constructor
+     */
     public User(){
 
     }
 
+    /**
+     * Constructor
+     * @param userName
+     * @param password
+     */
     public User(String userName, String password){
         this.userName=userName;
         this.password=password;
     }
 
+    /**
+     * Return loggedInUser which is static
+     * @return
+     */
     public static String getLoggedInUser() {
         return LoggedInUser;
     }
 
+    /**
+     * Set the loggedInUser which is static
+     * @param loggedInUser
+     */
     public static void setLoggedInUser(String loggedInUser) {
         LoggedInUser = loggedInUser;
     }
 
+    /**
+     * Return password of User.
+     * @return
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets password of User.
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Return userName of User
+     * @return
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * Set the userName of user.
+     * @param userName
+     */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    /**
+     * Overrides the equal method of the Object class.
+     * Checks if this User is equal to another User obj
+     * Return true if equal
+     * Return false if not equal
+     * @param obj other User object
+     * @return true if equal
+     * false if not equal.
+     */
     @Override
     public boolean equals(Object obj) {
         if(this.getUserName().equals(((User)obj).getUserName())) {
